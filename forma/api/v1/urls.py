7e465bcf43fb10996c275e2577b95af447d29a48
Carlos Vargas <varlopecar@gmail.com>
2025-05-23 15:17:24 +0200

    chore: test api

--- forma/api/v1/urls.py
+++ forma/api/v1/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from forma.api.v1.views import ItemViewSet
+from forma.api.v1.views import ItemViewSet, test_endpoint
 
 router = DefaultRouter()
 router.register(r'items', ItemViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('test/', test_endpoint, name='test-endpoint'),
 ]
--- forma/api/v1/views.py
+++ forma/api/v1/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from forma.models import Item
 from forma.api.v1.serializers import ItemSerializer
+
 
 class ItemViewSet(viewsets.ModelViewSet):
     """
@@ -9,3 +12,15 @@ class ItemViewSet(viewsets.ModelViewSet):
     """
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+
+
+@api_view(['GET'])
+def test_endpoint(request):
+    """
+    A simple test endpoint that returns a greeting message.
+    """
+    return Response({
+        'message': 'Hello from the test endpoint!',
+        'status': 'success',
+        'version': 'v1'
+    })
